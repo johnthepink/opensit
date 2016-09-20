922f94e0ab6bee3eef7828a6866d235d158a3c0c
Dan Bartlett <danbartlett@gmail.com>
2016-09-20 09:18:27 +0100

    Update db import script to use new pg:backups format

--- db/import_live.sh
+++ db/import_live.sh
@@ -1,4 +1,4 @@
-heroku pgbackups:capture --expire -a opensit
-curl -o latest.dump `heroku pgbackups:url -a opensit`
+heroku pg:backups capture -a opensit
+curl -o latest.dump `heroku pg:backups public-url -a opensit`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U dan -d opensit_development latest.dump
 rm latest.dump
