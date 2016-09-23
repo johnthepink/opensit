e3a548c5c3179cf2fa097b6f02412e3ae05281f6
Dan Bartlett <danbartlett@gmail.com>
2016-09-23 10:33:26 +0100

    Expand prune_spam to include akismet checks. Expand blacklist

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -36,6 +36,21 @@ describe 'Users' do
     expect(page.body).to include("Registration blocked")
   end
 
+  it 'does not block similar patterns' do
+    visit new_user_registration_path
+    fill_in "user_username", with: "newbie"
+    fill_in "user_email", with: "dan@somethingpl.com" # include pl, but not .pl
+    fill_in "user_password", with: "gunsbitchesbling"
+
+    within(".new_user") do
+      find_button("Sign up").trigger('click')
+    end
+
+    should have_content "Registration complete! Happy sitting."
+    should have_content "Welcome to our community!"
+    expect(page.body).to_not include("Registration blocked")
+  end
+
   describe 'registered users' do
 
     it 'should be able to log in' do
